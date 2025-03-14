f22bf451bef926d6831aea7530de6c069657ed93
rishkj <45304010+rishkj@users.noreply.github.com>
2025-03-14 15:27:49 -0700

    feat(FrontEnd): [SCRUM-24] Login and Signup Page Creation
    - Created login page
    - Created signup page
    SCRUM-24 #comment Add Login and Signup Page #done

---
+++ eventfe/src/login/Login 2.jsx
@@ -0,0 +1,56 @@
+import React, { createContext, useContext, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import './login.css'
+import { FormContextLogin } from "./FormContextLogin";
+
+const preventRefresh = (e) => {
+    e.preventDefault();
+};
+
+
+const Login = () => {
+    const { formDataLogin, setFormDataLogin} = useContext(FormContextLogin);
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormDataLogin(prevData => ({
+            ...prevData,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        navigate('/home');
+    };
+
+    return (
+        <div className="wrapper signIn">
+			{/* <div className="illustration">
+				<img src="https://source.unsplash.com/random" alt="illustration" />
+			</div> */}
+			<div className="form">
+				<div className="heading">LOGIN</div>
+				<form>
+					<div>
+						<label htmlFor="username">Username</label>
+						<input type="text" id="name" name="username" value={formDataLogin.username} placeholder="Enter your username" onChange={handleChange} />
+					</div>
+					<div>
+						<label htmlFor="password">Password</label>
+						<input type="text" id="password" name="password" value={formDataLogin.password} placeholder="Enter your password" onChange={handleChange} />
+					</div>
+					<button type="submit" onClick={handleSubmit}>
+						Submit
+					</button>
+				</form>
+				<p>
+					Don't have an account ? <Link to="/signup"> Sign In </Link>
+				</p>
+			</div>
+		</div>
+    )
+}
+
+export default Login;
---
+++ eventfe/src/navbar/navbar.jsx
@@ -0,0 +1,30 @@
+import React, { useContext, useState } from "react"
+import { Link } from "react-router-dom"
+import "./navbar.css"
+import { FormContextLogin } from "../login/FormContextLogin"
+
+const Navbar = () => {
+    const { formDataLogin } = useContext(FormContextLogin);
+
+    return (
+        <div class = "navbar">
+            <header>
+                <nav className='flexSB'>
+                    <ul class="flexSB">
+                        <li>
+                            <Link to='/home'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/allEvents'>{formDataLogin.username}</Link>
+                        </li>
+                        <li>
+                            <Link to='myTickets'>My Tickets</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default Navbar;
