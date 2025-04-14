271b1c7f08daa4066730c6c53395a551038ce7ed
rishkj <45304010+rishkj@users.noreply.github.com>
2025-04-13 18:28:44 -0700

    feat(FrontEnd): [SCRUM-25] Login and Signup Page API Integration
    Added API calls from React to the backend framework for signup and login functionality
    Included error messages for incorrect login or signup issues
    SCRUM-25 #comment Integrated API calls for login and signup #done

--- eventfe/src/login/Signup.jsx
+++ eventfe/src/login/Signup.jsx
@@ -1,25 +1,93 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { createContext, useContext, useState, useEffect } from "react"
+import { Link, useNavigate } from "react-router-dom";
+import { FormContextSignup } from "./FormContextSignup";
+import { signup } from "../api/auth";
+
+const preventRefresh = (e) => {
+  e.preventDefault();
+};
+
 
 const Signup = () => {
+  const { formDataSignup, setFormDataSignup } = useContext(FormContextSignup);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const [showError, setShowError] = useState(false);
+  const [errorText, setErrorText] = useState("");
+  
+  useEffect(() => {
+    if (showError) {
+      const timer = setTimeout(() => setShowError(false), 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [showError]);
+  
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormDataSignup(prevData => ({
+        ...prevData,
+        [name]: value
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+      e.preventDefault();
+      setError(null);
+  
+      try {
+        console.log(formDataSignup.type);
+        const response = await signup(formDataSignup.name, formDataSignup.email, formDataSignup.password, formDataSignup.type);
+  
+        if(response.ok) {
+          console.log("Signed up successfully");
+          navigate('/');
+        }
+
+        else {
+          const errorMessage = response.data.message;
+          setErrorText(errorMessage);
+          setShowError(true);
+          
+        }
+        
+      } catch (err) {
+        console.error("Login Error: ", err.message);
+        setError(err.message);
+      }
+          
+  };
+
   return (
     <div className="wrapper signUp">
-      {/* <div className="illustration">
-        <img src="https://source.unsplash.com/random" alt="illustration" />
-      </div> */}
+
       <div className="form">
         <div className="heading">CREATE AN ACCOUNT</div>
+
+        {showError && (
+					<p style={{ color: "red", marginBottom: "1rem", fontWeight: "bold" }}>
+						{errorText}
+					</p>
+				)}
+
         <form>
           <div>
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" placeholder="Enter your username" />
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name" value={formDataSignup.name} placeholder="Enter your name" onChange={handleChange}/>
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" name="email" value={formDataSignup.email} placeholder="Enter your email" onChange={handleChange}/>
           </div>
           <div>
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" placeholder="Enter you password" />
+            <input type="password" id="password" name="password" value={formDataSignup.password} placeholder="Enter you password" onChange={handleChange}/>
           </div>
-          <button type="submit">Submit</button>
-          <h2 align="center" class="or">
+          <button type="submit" onClick={handleSubmit}>
+            Submit
+          </button>
+          <h2 align="center" className="or">
             OR
           </h2>
         </form>
